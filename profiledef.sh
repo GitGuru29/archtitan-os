5730a2629225d0408b4543132ee6b66e2c63473a
GitGuru29 <sdangalla66@gmail.com>
2026-08-30 15:38:05 +0530

    fix(build): remove -processors 0 from mksquashfs options to use all available cores by default

--- profiledef.sh
+++ profiledef.sh
@@ -11,7 +11,7 @@ buildmodes=('iso')
 bootmodes=('uefi.grub')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '19' '-b' '1M' '-processors' '0' '-mem' '4G')
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '19' '-b' '1M' '-mem' '4G')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
