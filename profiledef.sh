d5753035f53c70407674070dd8eaa95f3577ebe7
GitGuru29 <sdangalla66@gmail.com>
2026-08-31 18:05:25 +0530

    chore: generate build artifacts and configuration for titan-media-hud

--- profiledef.sh
+++ profiledef.sh
@@ -23,6 +23,7 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/launch-installer"]="0:0:755"
   ["/usr/local/bin/titan_hw_manager"]="0:0:755"
   ["/usr/local/bin/titan-hwm"]="0:0:755"
   ["/usr/local/bin/titan-hwm-waybar"]="0:0:755"
