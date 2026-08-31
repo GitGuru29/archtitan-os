0315cc218ecaf8a55a7b2be45e84256927805c1b
GitGuru29 <sdangalla66@gmail.com>
2026-08-31 21:37:56 +0530

    fix(iso): fix titanbrowser and archtitan-settings permissions and restore graphical GRUB boot menu

--- profiledef.sh
+++ profiledef.sh
@@ -42,6 +42,7 @@ file_permissions=(
   ["/usr/local/bin/archtitan-postinstall"]="0:0:755"
   ["/etc/systemd/system/archtitan-postinstall@.service"]="0:0:644"
   ["/usr/bin/titanfetch"]="0:0:755"
+  ["/usr/bin/titanbrowser"]="0:0:755"
   ["/usr/bin/archtitan-settings"]="0:0:755"
   ["/etc/systemd/system/titan-active.slice"]="0:0:644"
   ["/etc/systemd/system/titan-background.slice"]="0:0:644"
